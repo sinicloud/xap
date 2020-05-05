32fbb4fa7cfb6791aa40aefe847f37b82c0ea361
nnszsqddw <2586812750@qq.com>
2020-05-05 18:17:19 +0800

    fix Constant audio rate

--- Demo/Java/src/com/xap/voicetranslate/Constant.java
+++ Demo/Java/src/com/xap/voicetranslate/Constant.java
@@ -1,7 +1,7 @@
 package com.xap.voicetranslate;
 
 public class Constant {
-    public static int AUDIO_RATE = 16000;
+    public static final int AUDIO_RATE = 16000;
     public static final String APP_ID = "xapaccount-1385487646";
     public static final String APP_SECRET = "b1603e2fad947fc1a560e1f4bb7ffd589f2fbd785ca5176758ec0c9834ac4e3f";
     public static final String WSS_URL = "wss://api.xap.sinicloud.com:16443/v1/xap/?appID=%1s&salt=%2s" +
